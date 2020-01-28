4ca5bf8246657e18f9273f5d5bc64ba77c7e9ba4
Bryan Yaggi <bryan.yaggi@gmail.com>
2020-01-27 19:47:49 -0800

    Completed PA3.

---
+++ course2/pa3.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+
+import time
+import heapq
+
+'''
+Programming Assignment 3
+
+Download the following text file: median.txt
+
+The goal of this problem is to implement the "Median Maintenance" algorithm
+(covered in the Week 3 lecture on heap applications). The text file contains a
+list of the integers from 1 to 10000 in unsorted order; you should treat this as
+a stream of numbers, arriving one by one. Letting x_i denote the ith number of
+the file, the kth median m_k is defined as the median of the numbers x_1, ...,
+x_k. (So, if k is odd, then m_k is ((k + 1)/2)th smallest number among x1, ...,
+x_k; if k is even, then m_k is the (k/2)th smallest number among x1, ..., x_k.)
+
+In the box below you should type the sum of these 10000 medians, modulo 10000
+(i.e., only the last 4 digits). That is, you should compute
+(m_1 + m_2 + m_3+ ... + m_10000) mod 10000.
+
+OPTIONAL EXERCISE: Compare the performance achieved by heap-based and
+search-tree-based implementations of the algorithm.
+'''
+
+'''
+Class that implement median maintenance algorithm.
+'''
+class MedianMaintainer:
+    def __init__(self, stream):
+        self.stream = stream[::-1] # reverse list to pop from end
+        self.minHeap = []
+        self.maxHeap = []
+        self.medians = []
+
+    def pushMinHeap(self, element):
+        heapq.heappush(self.minHeap, element)
+
+    def pushMaxHeap(self, element):
+        heapq.heappush(self.maxHeap, -element)
+
+    def popMinHeap(self):
+        return heapq.heappop(self.minHeap)
+
+    def popMaxHeap(self):
+        return -heapq.heappop(self.maxHeap)
+
+    def peekMinHeap(self):
+        return self.minHeap[0]
+
+    def peekMaxHeap(self):
+        return -self.maxHeap[0]
+
+    def insertElement(self, element):
+        if len(self.maxHeap) == 0:
+            self.pushMaxHeap(element)
+        elif element <= self.peekMaxHeap():
+            self.pushMaxHeap(element)
+        else:
+            self.pushMinHeap(element)
+
+    def balanceHeaps(self):
+        balanced = False
+
+        while not balanced:
+            maxHeapSize = len(self.maxHeap)
+            minHeapSize = len(self.minHeap)
+
+            if (maxHeapSize + minHeapSize) % 2 == 0: # even
+                if maxHeapSize - minHeapSize >= 2:
+                    element = self.popMaxHeap()
+                    self.pushMinHeap(element)
+                elif minHeapSize - maxHeapSize >= 3:
+                    element = self.popMinHeap()
+                    self.pushMaxHeap(element)
+                else:
+                    balanced = True
+            else: # odd
+                if maxHeapSize - minHeapSize >= 3:
+                    element = self.popMaxHeap()
+                    self.pushMinHeap(element)
+                elif minHeapSize - maxHeapSize >= 3:
+                    element = self.popMinHeap()
+                    self.pushMaxHeap(element)
+                else:
+                    balanced = True
+
+        return maxHeapSize, minHeapSize
+
+    def getMedian(self):
+        maxHeapSize, minHeapSize = self.balanceHeaps()
+        median = 0
+
+        if (maxHeapSize + minHeapSize) % 2 == 0: # even
+            if maxHeapSize == minHeapSize:
+                median = self.peekMaxHeap()
+            elif minHeapSize - maxHeapSize == 2:
+                median = self.peekMinHeap()
+            else:
+                print('Unbalanced heaps!')
+        else: # odd
+            if maxHeapSize - minHeapSize == 1:
+                median = self.peekMaxHeap()
+            elif minHeapSize - maxHeapSize == 1:
+                median = self.peekMinHeap()
+            else:
+                print('Unbalanced heaps!')
+
+        return median
+
+    def processNext(self):
+        element = self.stream.pop()
+        self.insertElement(element)
+        return self.getMedian()
+
+    def processStream(self, verbose=False):
+        if verbose:
+            print('stream = %s' %self.stream)
+
+        while len(self.stream):
+            self.medians.append(self.processNext())
+            if verbose:
+                print('min heap: %s' %self.minHeap)
+                print('max heap: %s' %self.maxHeap)
+
+        if verbose:
+            print('medians = %s' %self.medians)
+
+    def calcResult(self):
+        summation = 0
+        for median in self.medians:
+            summation += median
+        return summation % 10000
+
+def readFile(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+    stream = []
+    for line in lines:
+        stream.append(int(line))
+
+    return stream
+
+def testMedianMaintainer():
+    #stream = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    #stream = [1, 666, 10, 667, 100, 2, 3]
+    stream = [6331, 2793, 1640, 9290, 225, 625, 6195, 2303, 5685, 1354]
+    return MedianMaintainer(stream)
+
+if __name__ == '__main__':
+    t0 = time.time()
+    #mm = testMedianMaintainer()
+    stream = readFile('median.txt')
+    mm = MedianMaintainer(stream)
+    mm.processStream()
+    print('result = %s' %mm.calcResult())
+    print('total time = %f' %(time.time() - t0))
