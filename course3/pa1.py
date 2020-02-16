049283d9f0de954c156d2b7c4e73448f2a988af5
Bryan Yaggi <bryan.yaggi@gmail.com>
2020-02-15 18:50:19 -0800

    Completed PA1 P3.

--- course3/pa1.py
+++ course3/pa1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import time
+import random
+from heapdict import heapdict
 
 '''
 Programming Assignment 1
@@ -51,55 +53,215 @@ matter how you break ties. You should report the sum of weighted completion
 times of the resulting schedule --- a positive integer --- in the box below. 
 '''
 
-def readFile(filename):
-    with open(filename) as f:
-        lines = f.readlines()
+'''
+Class for scheduling.
+'''
+class Scheduler:
+    def __init__(self, filename):
+        self.jobs = self.readFile(filename)
 
-    jobs = []
-    for i in range(len(lines)):
-        if i != 0: # skip first line
-            line = lines[i].split()
-            job = {'jid': i}
-            job['weight'] = int(line[0])
-            job['length'] = int(line[1])
-            jobs.append(job)
+    def readFile(self, filename):
+        with open(filename) as f:
+            lines = f.readlines()
 
-    return jobs
+        jobs = []
+        for i in range(len(lines)):
+            if i != 0: # skip first line
+                line = lines[i].split()
+                job = {'jid': i}
+                job['weight'] = int(line[0])
+                job['length'] = int(line[1])
+                jobs.append(job)
 
-def calcP1Score(jobs):
-    for job in jobs:
-        score = job['weight'] - job['length']
-        job['p1Score'] = score
+        return jobs
 
-def calcP2Score(jobs):
-    for job in jobs:
-        score = job['weight'] / job['length']
-        job['p2Score'] = score
+    '''
+    Calculates Problem 1 score for each job.
+    '''
+    def calcP1Score(self):
+        for job in self.jobs:
+            score = job['weight'] - job['length']
+            job['p1Score'] = score
 
-def calcSumWeightedCompletions(jobs):
-    result = 0
-    compTime = 0
-    for job in jobs:
-        compTime += job['length']
-        result += job['weight'] * compTime
+    '''
+    Calculates Problem 2 score for each job.
+    '''
+    def calcP2Score(self):
+        for job in self.jobs:
+            score = job['weight'] / job['length']
+            job['p2Score'] = score
 
-    return result
+    '''
+    Calculates sum of weighted completion times for given job order.
+    sortedJobs is sorted list of jobs
+    '''
+    def calcSumWeightedCompletions(self, sortedJobs):
+        result = 0
+        compTime = 0
+        for job in sortedJobs:
+            compTime += job['length']
+            result += job['weight'] * compTime
 
-def calcP1Result(jobs):
-    calcP1Score(jobs)
-    jobsSorted = sorted(jobs, key = lambda x: (x['p1Score'], x['weight']),
-            reverse=True)
-    return calcSumWeightedCompletions(jobsSorted)
+        return result
 
-def calcP2Result(jobs):
-    calcP2Score(jobs)
-    jobsSorted = sorted(jobs, key = lambda x: x['p2Score'], reverse=True)
-    return calcSumWeightedCompletions(jobsSorted)
+    '''
+    Calculates Problem 1 result.
+    '''
+    def calcP1Result(self):
+        self.calcP1Score()
+        jobsSorted = sorted(self.jobs,
+                key = lambda x: (x['p1Score'], x['weight']), reverse=True)
+        return self.calcSumWeightedCompletions(jobsSorted)
+
+    '''
+    Calculates Problem 2 result.
+    '''
+    def calcP2Result(self):
+        self.calcP2Score()
+        jobsSorted = sorted(self.jobs,
+                key = lambda x: x['p2Score'], reverse=True)
+        return self.calcSumWeightedCompletions(jobsSorted)
+
+def runSchedulingProblems():
+    t0 = time.time()
+    scheduler = Scheduler('jobs.txt')
+    print('p1 result = %s' %scheduler.calcP1Result())
+    print('p2 result = %s' %scheduler.calcP2Result())
+    print('p1 and p2 time = %f' %(time.time() - t0))
+
+'''
+Problem 3
+
+In this programming problem you'll code up Prim's minimum spanning tree
+algorithm.
+
+Download the following text file: edges.txt
+
+This file describes an undirected graph with integer edge costs. It has the
+format:
+
+[number_of_nodes] [number_of_edges]
+
+[one_node_of_edge_1] [other_node_of_edge_1] [edge_1_cost]
+
+[one_node_of_edge_2] [other_node_of_edge_2] [edge_2_cost]
+
+...
+
+For example, the third line of the file is "2 3 -8874", indicating that there is
+an edge connecting vertex #2 and vertex #3 that has cost -8874.
+
+You should NOT assume that edge costs are positive, nor should you assume that
+they are distinct.
+
+Your task is to run Prim's minimum spanning tree algorithm on this graph. You
+should report the overall cost of a minimum spanning tree --- an integer, which
+may or may not be negative --- in the box below.
+
+IMPLEMENTATION NOTES: This graph is small enough that the straightforward O(mn)
+time implementation of Prim's algorithm should work fine. OPTIONAL: For those of
+you seeking an additional challenge, try implementing a heap-based version. The
+simpler approach, which should already give you a healthy speed-up, is to
+maintain relevant edges in a heap (with keys = edge costs). The superior
+approach stores the unprocessed vertices in the heap, as described in lecture.
+Note this requires a heap that supports deletions, and you'll probably need to
+maintain some kind of mapping between vertices and their positions in the heap.
+'''
+
+'''
+Class for running Prim's algorithm on a graph.
+'''
+class Graph:
+    def __init__(self, filename=None, maxCost=1000000):
+        self.nodes = {}
+        self.mst = []
+        if filename is not None:
+            self.readFilename(filename)
+        self.maxCost = maxCost
+
+    def readFilename(self, filename):
+        with open(filename) as f:
+            lines = f.readlines()
+
+        for i in range(len(lines)):
+            if i != 0:
+                line = lines[i].split()
+                self.addEdge(int(line[0]), int(line[1]), int(line[2]))
+
+    '''
+    Adds a node to the graph.
+    node is new node
+    '''
+    def addNode(self, node):
+        if node not in self.nodes:
+            self.nodes[node] = {}
+
+    '''
+    Adds an edge to the graph.
+    node1 is first node
+    node2 is second node
+    cost is edge cost
+    '''
+    def addEdge(self, node1, node2, cost):
+        self.addNode(node1)
+        self.addNode(node2)
+        self.nodes[node1][node2] = cost
+        self.nodes[node2][node1] = cost
+
+    '''
+    Runs Prim's algorithm to find a MST.
+    verbose prints the heap contents on each iteration
+    '''
+    def prims(self, verbose=False):
+        '''
+        Prints heapdict contents.
+        '''
+        def printHeap(heap):
+            print('heap contents:')
+            for element in heap:
+                print(element, heap[element])
+
+        startNode = random.choice(list(self.nodes))
+        explored = {startNode}
+        self.mst = [(startNode, 0)]
+        unexplored = heapdict()
+        for node in self.nodes:
+            if node not in explored:
+                unexplored[node] = self.maxCost
+        if verbose:
+            printHeap(unexplored)
+
+        newNode = startNode
+        while(unexplored): # not empty
+            # Update heap
+            for uNode in unexplored:
+                if newNode in self.nodes[uNode]:
+                    newCost = self.nodes[uNode][newNode]
+                    if newCost < unexplored[uNode]:
+                        unexplored[uNode] = newCost
+            if verbose:
+                printHeap(unexplored)
+
+            # Extract new node and cost, add to MST
+            newNode, cost = unexplored.popitem()
+            self.mst.append((node, cost))
+
+    '''
+    Calculates MST path cost.
+    '''
+    def calcMstCost(self):
+        totalCost = 0
+        for edge in self.mst:
+            totalCost += edge[1]
+        return totalCost
+
+def runPrimsProblem():
+    t0 = time.time()
+    graph = Graph('edges.txt')
+    graph.prims()
+    print('p3 result = %s' %graph.calcMstCost())
+    print('p3 time = %f' %(time.time() - t0))
 
 if __name__ == '__main__':
-    t0 = time.time()
-    jobs = readFile('jobs.txt')
-    p1Result = calcP1Result(jobs)
-    print('p1 result = %s' %calcP1Result(jobs))
-    print('p2 result = %s' %calcP2Result(jobs))
-    print('total time = %f' %(time.time() - t0))
+    runSchedulingProblems()
+    runPrimsProblem()
