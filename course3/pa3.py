138cdcdaf57a5df2501e7b920f314574d0a9095d
Bryan Yaggi <bryan.yaggi@gmail.com>
2020-03-01 20:54:25 -0800

    Completed PA3.

--- course3/pa3.py
+++ course3/pa3.py
@@ -60,9 +60,11 @@ class HuffmanCode:
     def __init__(self, filename):
         self.codes = {}
         self.heap = []
-        self.readFile(filename)
+        self._readFile(filename)
+        self._createTree()
+        self._generateCodes()
 
-    def readFile(self, filename):
+    def _readFile(self, filename):
         with open(filename) as f:
             lines = f.readlines()
 
@@ -73,7 +75,7 @@ class HuffmanCode:
                 self.heap.append(node)
         heapq.heapify(self.heap)
 
-    def createTree(self):
+    def _createTree(self):
         if len(self.heap) < 2:
             print('Symbol list too short.')
             return
@@ -85,7 +87,7 @@ class HuffmanCode:
                 heapq.heappush(self.heap,
                         Node(weight, leftNode=left, rightNode=right))
 
-    def createCodes(self):
+    def _generateCodes(self):
         def recurse(node, code):
             if node.symbol is not None:
                 self.codes[code] = node.symbol
@@ -99,15 +101,104 @@ class HuffmanCode:
         code = ''
         recurse(node, code)
         
-    def printResults(self):
+    def printBitLengthRange(self):
         codes = list(self.codes.keys())
         codes = sorted(codes, key=len)
         minBits = len(codes[0])
         maxBits = len(codes[-1])
         print('max length = %s, min length = %s' %(maxBits, minBits))
 
-if __name__ == '__main__':
+def runHuffmanProblem():
+    t0 = time.time()
     huffmanCode = HuffmanCode('huffman.txt')
-    huffmanCode.createTree()
-    huffmanCode.createCodes()
-    huffmanCode.printResults()
+    huffmanCode.printBitLengthRange()
+    print('huffman problem time = %s' %(time.time() - t0))
+
+'''
+Problem 3
+
+In this programming problem you'll code up the dynamic programming algorithm for
+computing a maximum-weight independent set of a path graph.
+
+Download the following text file: mwis.txt
+
+This file describes the weights of the vertices in a path graph (with the
+weights listed in the order in which vertices appear in the path). It has the
+following format:
+
+[number_of_vertices]
+
+[weight of first vertex]
+
+[weight of second vertex]
+
+...
+
+For example, the third line of the file is "6395702," indicating that the weight
+of the second vertex of the graph is 6395702.
+
+Your task in this problem is to run the dynamic programming algorithm (and the
+reconstruction procedure) from lecture on this data set. The question is: of the
+vertices 1, 2, 3, 4, 17, 117, 517, and 997, which ones belong to the
+maximum-weight independent set? (By "vertex 1" we mean the first vertex of the
+graph---there is no vertex 0.) In the box below, enter a 8-bit string, where the
+ith bit should be 1 if the ith of these 8 vertices is in the maximum-weight
+independent set, and 0 otherwise. For example, if you think that the vertices 1,
+4, 17, and 517 are in the maximum-weight independent set and the other four
+vertices are not, then you should enter the string 10011010 in the box below.
+'''
+
+class Mwis:
+    def __init__(self, filename):
+        self._readFile(filename)
+        self._calcValue()
+        self._findMembers()
+
+    def _readFile(self, filename):
+        with open(filename) as f:
+            lines = f.readlines()
+
+        for i in range(len(lines)):
+            if i == 0:
+                numVertices = int(lines[i])
+                self.vertices = [0] * (numVertices+1)
+                self.A = [0] * (numVertices+1)
+                self.members = [0] * (numVertices+1)
+            else:
+                self.vertices[i] = int(lines[i])
+            self.A[1] = self.vertices[1]
+
+    def _calcValue(self):
+        for i in range(2, len(self.vertices)):
+            self.A[i] = max(self.A[i-1], self.A[i-2] + self.vertices[i])
+        self.value = self.A[-1]
+
+    def _findMembers(self):
+        i = len(self.A) - 1
+        while i >= 1:
+            if self.A[i-1] >= self.A[i-2] + self.vertices[i]:
+                i -= 1
+            else:
+                self.members[i] = 1
+                i -= 2
+
+    def printResult(self):
+        vertices = [1, 2, 3, 4, 17, 117, 517, 997]
+        result = ''
+        for vertex in vertices:
+            if vertex >= len(self.members):
+                result += '0'
+            else:
+                result += str(self.members[vertex])
+        
+        print(result)
+
+def runMwisProblem():
+    t0 = time.time()
+    mwis = Mwis('mwis.txt')
+    mwis.printResult()
+    print('mwis problem time = %s' %(time.time() - t0))
+
+if __name__ == '__main__':
+    runHuffmanProblem()
+    runMwisProblem()
